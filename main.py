00f1b040278a786874103e34daa9c87141727916
Rolerdxx <azbilal.motassim@gmail.com>
2023-04-14 18:06:10 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+import pyqtcss
+
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("tabw.ui",self)
+        self.tableWidget.setColumnWidth(0,250)
+        self.tableWidget.setColumnWidth(1,100)
+        self.tableWidget.setColumnWidth(2,350)
+        self.loaddata()
+        self.pushButton.clicked.connect(self.click)
+
+    def loaddata(self):
+        people=[{"name":"John","age":45,"address":"New York"}, {"name":"Mark", "age":40,"address":"LA"},
+                {"name":"George","age":30,"address":"London"}]
+        row = 0
+        self.tableWidget.setRowCount(len(people))
+        for person in people:
+            self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(person["name"]))
+            self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(str(person["age"])))
+            self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(person["address"]))
+            row = row + 1
+    def click(self):
+        print("woo")
+
+stylesheet = """
+        MainWindow {
+            background-image: url("D:/mainimage.jpg"); 
+            background-repeat: no-repeat; 
+            background-position: center;
+        }
+"""
+
+style_string = pyqtcss.get_style("dark_blue")
+app = QApplication(sys.argv)
+app.setStyleSheet(style_string)
+mainwindow = MainWindow()
+mainwindow.setStyleSheet(stylesheet)
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedHeight(600)
+widget.setFixedWidth(1067)
+widget.show()
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
