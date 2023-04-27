057f3d462443973bddee159a8312cd3c656ed296
Rolerdxx <azbilal.motassim@gmail.com>
2023-04-27 18:31:09 +0100

    fixed reservation

--- Pages/CarPage.py
+++ Pages/CarPage.py
@@ -3,7 +3,8 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog
 from Helpers.ImageLabel import getImageLabel
 from PyQt5.QtGui import QPixmap
-from Pages.reservation import resevationPage
+from Pages.reservation import ReservationDialog
+
 
 class CarPage(QDialog):
     def __init__(self, main):
@@ -28,13 +29,13 @@ class CarPage(QDialog):
         self.placeslabel.setText(str(self.car[4]))
         self.transmissionlabel.setText(self.car[5])
         self.statelabel.setText(str(self.car[6]))
-        self.pricelabel.setText(str(self.car[7])+" DH")
+        self.pricelabel.setText(str(self.car[7]) + " DH")
 
     def reserveit(self):
-        state=self.statelabel.text()
-        price=self.pricelabel.text()
-        if state=="1":
-            self.Dialog = QtWidgets.QDialog()
-            self.ui = resevationPage()
-            self.ui.setupUi(self.Dialog)
-            self.Dialog.show()
+        state = self.statelabel.text()
+        price = self.pricelabel.text()
+        if state == "1":
+            reservationpage = ReservationDialog()
+            res = reservationpage.exec()
+            if res:
+                print("user clicked ok")
