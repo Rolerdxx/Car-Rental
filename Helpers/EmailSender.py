e033752115d5d0d46e160c8ab348c903b83d1c07
Rolerdxx <azbilal.motassim@gmail.com>
2023-04-27 16:05:16 +0100

    Stuff

--- Helpers/EmailSender.py
+++ Helpers/EmailSender.py
@@ -1,12 +1,19 @@
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+import os
+
 
 
 def SendEmail(towho, subject, message):
+    print(towho)
+    print(subject)
+    print(message)
     message = Mail(
         from_email='carrentalapp123@gmail.com',
         to_emails=towho,
         subject=subject,
         html_content=message)
-    sg = SendGridAPIClient('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')
-    sg.send(message)
+    print(os.environ.get('SENDGRID_API_KEY'))
+    sg = SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code, response.body, response.headers)
