28373b076dbe61a68de4de69e24eff62507dc6e0
Rolerdxx <azbilal.motassim@gmail.com>
2023-05-02 21:27:18 +0100

    @

--- Pages/CarPage.py
+++ Pages/CarPage.py
@@ -36,8 +36,7 @@ class CarPage(QDialog):
         self.pricelabel.setText(str(self.car[8]) + " DH")
 
     def reserveit(self):
-        state = self.car[7]
-        print(state)
+        state = str(self.car[7])
         if state == "1":
             revdialog = ReservationDialog()
             res = revdialog.exec()
